48287959ad13caac9b49e70764b7929a98e340b6
sersanrr <ltrsa@yandex.ru>
2016-09-12 05:58:01 +0000

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ gem 'rails', '4.2.6'
 #Bootstrap
 gem 'bootstrap-sass'
 gem 'bcrypt'
+gem 'faker'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,6 +14,10 @@ module SessionsHelper
         remember_token = User.encrypt(cookies[:remember_token])
         @current_user ||= User.find_by(remember_token: remember_token)
     end
+    
+    def current_user?(user)
+        user == current_user
+    end
 
 
     def signed_in?
@@ -25,6 +29,15 @@ module SessionsHelper
                                   User.encrypt(User.new_remember_token))
         cookies.delete(:remember_token)
         self.current_user = nil
+    end
+    
+    def redirect_back_or(default)
+        redirect_to(session[:return_to] || default)
+        session.delete(:return_to)
+    end
+
+    def store_location
+        session[:return_to] = request.url if request.get?
     end
   
 end
